34ddaacf1c74051796848ec744e09fc4cc533a8b
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-09-03 15:27:25 +0530

    fixed typo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Steps to run 
+
+## change the config.py and add db url
+
+## set venv and import the requirements
+
+### run the main.py 
+
--- extract.py
+++ extract.py
@@ -4,14 +4,15 @@ from config import DB_CONNECTION_STRING
 
 def get_engine():
     engine = create_engine(DB_CONNECTION_STRING)
+    return engine
 
 def extract_product_offering(engine):
     query="SELECT id,description,unit_type,hsn_sac_code,valid_end_datetime FROM product_offering"
     return pd.read_sql(query,engine)
 
 def extract_product_offering_price(engine):
-    query="SELECT id, name, tax_schema_code from product_offering_price"
-    return pd.read_sql(query,engine)
+    query = "SELECT id, product_offering_id, name, tax_scheme_code FROM product_offering_price"
+    return pd.read_sql(query, engine)
 
 def extract_product_offering_characteristic(engine):
     query="SELECT id, product_offering_id,characteristic_code_id, characteristic_value from product_offering_characteristic"
